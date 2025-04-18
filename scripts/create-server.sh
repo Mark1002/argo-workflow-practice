3bec4c940c90d70eeb19ee5b094a1f6e400feab7
Mark Chang <ak0922747401@gmail.com>
2025-04-18 09:10:12 +0800

    add date parameter and backfill example

--- scripts/create-server.sh
+++ scripts/create-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-$ curl -s https://raw.githubusercontent.com/argoproj-labs/training-material/main/argo-workflows/install.sh | sh
+curl -s https://raw.githubusercontent.com/argoproj-labs/training-material/main/argo-workflows/install.sh | sh
