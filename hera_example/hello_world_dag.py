5406a59292878e75f118a99dbd159b9917cc60e9
Mark Chang <ak0922747401@gmail.com>
2025-06-20 15:43:52 +0800

    add exit-handler

--- hera_example/hello_world_dag.py
+++ hera_example/hello_world_dag.py
@@ -1,5 +1,7 @@
 import os
-from hera.workflows import DAG, Workflow, WorkflowsService, script
+from hera.workflows import Steps, DAG, Workflow, WorkflowsService, script
+
+from hera_example.utils import argo_lint_from_yaml
 
 ARGO_SERVER = os.getenv("ARGO_SERVER", "http://localhost:2746")
 ARGO_TOKEN = os.getenv("ARGO_TOKEN")
@@ -7,7 +9,14 @@ ARGO_TOKEN = os.getenv("ARGO_TOKEN")
 
 @script(image="python:3.11-alpine")
 def echo(message: str):
+    if message == "C":
+        raise ValueError("Simulating a failure in task C")
     print(message)
+
+
+@script(image="python:3.11-alpine")
+def notify():
+    print("Exiting workflow and sending notification.")
 
 
 with Workflow(
@@ -16,15 +25,23 @@ with Workflow(
     namespace="argo",
     workflows_service=WorkflowsService(host=ARGO_SERVER, token=ARGO_TOKEN)
 ) as w:
-    with DAG(name="hello-dag"):
+    with Steps(name="exit-handler") as exit_handler:
+        notify(name="exit-handler", when="{{workflow.status}} != Succeeded")
+
+    with DAG(name="hello-dag") as hello_dag:
         A = echo(name="A", arguments={"message": "A"})
         B = echo(name="B", arguments={"message": "B"})
         C = echo(name="C", arguments={"message": "C"})
         D = echo(name="D", arguments={"message": "D"})
+
         A >> [B, C] >> D
 
+    w.on_exit = exit_handler
+
 # Compile Hera python code to Argo Workflow yaml
-print(w.to_yaml())
+yaml = w.to_yaml()
+print(yaml)
+argo_lint_from_yaml(yaml)
 # Create the workflow in the Argo server
 submitted_workflow = w.create()
 print(f"Workflow at {ARGO_SERVER}/workflows/argo/{submitted_workflow.metadata.name}") # noqa
